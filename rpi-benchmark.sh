8f089a97f523c72cd8912e6ad0d3edaf71511464
Emanuele Ballarin <emanuele@ballarin.cc>
2022-06-19 16:26:45 +0200

    Update rpi-benchmark.sh

--- rpi-benchmark.sh
+++ rpi-benchmark.sh
@@ -13,7 +13,7 @@ fi
 # Skip the speed test if the user doesn't want it to run
 case $1 in
     --no-speedtest|-ns )
-        echo "Skipping speed test"
+        echo "Skipping speed test."
         echo
         ;;
     * )
@@ -30,8 +30,8 @@ ROOTDISK=`mount | grep " on / type" | cut -f 1 -d " "`
 clear
 sync
 echo -e "\e[96mRaspberry Pi Benchmark Test"
-echo -e "Author: AikonCWD"
-echo -e "Version: 3.0\n\e[97m"
+echo -e "Authors: AikonCWD, Robert McKenzie, Aaron Covrig, Emanuele Ballarin"
+echo -e "Version: 3.0.3\n\e[97m"
 
 # Show current hardware
 vcgencmd measure_temp
@@ -46,40 +46,40 @@ echo -e "\n\e[93m"
 # Skip the speed test if the user doesn't want it to run
 case $1 in
     --no-speedtest|-ns )
-        echo "Skipping speed test"
+        echo "Skipping speed test."
         echo
         ;;
     * )
-	# Identify the installed version of speedtest
-	SPEEDTEST=``
-	if [ `which speedtest` ]; then
-		SPEEDTEST="$(which speedtest) --progress=yes"
-	elif [ `which speedtest-cli` ]; then
-		SPEEDTEST="$(which speedtest-cli) --simple"
-	else
-		echo -e "Failed to identify installed speedtest software\e[94m"
-	fi
-	if [ ! -z "$SPEEDTEST" ]; then
-		echo -e "Internet connection speed test will proceed with $SPEEDTEST\e[94m"
-        	echo -e "Running InternetSpeed test...\e[94m"
-		eval $SPEEDTEST
-        	echo -e "\e[93m"
-	fi
-	;;
+        # Identify the installed version of speedtest
+        SPEEDTEST=``
+        if [ `which speedtest` ]; then
+                SPEEDTEST="$(which speedtest) --progress=yes"
+        elif [ `which speedtest-cli` ]; then
+                SPEEDTEST="$(which speedtest-cli) --simple"
+        else
+                echo -e "Failed to identify installed speedtest software\e[94m"
+        fi
+        if [ ! -z "$SPEEDTEST" ]; then
+                echo -e "Internet connection speed test will proceed with $SPEEDTEST\e[94m"
+                echo -e "Running InternetSpeed test...\e[94m"
+                eval $SPEEDTEST
+                echo -e "\e[93m"
+        fi
+        ;;
 esac
 
 echo -e "Running CPU test...\e[94m"
-sysbench --num-threads=4 --validate=on --test=cpu --cpu-max-prime=5000 run | grep 'total time:\|min:\|avg:\|max:' | tr -s [:space:]
+sysbench --num-threads=4 --validate=on --cpu-max-prime=5000 cpu run | grep 'total time:\|min:\|avg:\|max:' | tr -s [:space:]
 vcgencmd measure_temp
 echo -e "\e[93m"
 
 echo -e "Running THREADS test...\e[94m"
-sysbench --num-threads=4 --validate=on --test=threads --thread-yields=4000 --thread-locks=6 run | grep 'total time:\|min:\|avg:\|max:' | tr -s [:space:]
+sysbench --num-threads=4 --validate=on --thread-yields=4000 --thread-locks=6 threads run | grep 'total time:\|min:\|avg:\|max:' | tr -s [:space:]
 vcgencmd measure_temp
 echo -e "\e[93m"
 
 echo -e "Running MEMORY test...\e[94m"
-sysbench --num-threads=4 --validate=on --test=memory --memory-block-size=1K --memory-total-size=3G --memory-access-mode=seq run | grep 'Operations\|transferred\|total time:\|min:\|avg:\|max:' | tr -s [:space:]
+sysbench --num-threads=4 --validate=on --memory-block-size=1K --memory-total-size=3G --memory-access-mode=seq memory run | grep 'Operations\|transferred\|total time:\|min:\|avg:\|max:' | tr -s [:space:]
 vcgencmd measure_temp
 echo -e "\e[93m"
 
@@ -99,4 +99,4 @@ vcgencmd measure_temp
 rm -f ~/test.tmp
 echo -e "\e[0m"
 
-echo -e "\e[91mAikonCWD's rpi-benchmark completed!\e[0m\n"
+echo -e "\e[91mrpi-benchmark completed!\e[0m\n"
